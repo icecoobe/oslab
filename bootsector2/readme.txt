ded3b888ae06e846e6e5253ec836ac4c67b95365
icecoobe <icecoobe@qq.com>
2015-07-17 17:41:30 +0800

    引导第2扇区的内容，从MBR移交控制权给该扇区程序

---
+++ bootsector2/readme.txt
@@ -0,0 +1,35 @@
+
+Boot sector-2
+===========
+
+We did practices on stage-1: MBR code for a long time. Now it's time to move on.
+
+This demo is a beginning where we will try to load data and programs into RAM. After that, we will hand the control to the loaded programs. Just like a stage2-bootblock program.
+
+## What does this  project do?
+This project contains two parts: boot.asm and sector2.asm.
+boot code will load the sector2 code into RAM, then jmp to where sector2 code lay.
+
+##boot.asm
+It's like boot.s in linux 0.11, and is a MBR code. This code will be first loaded when pc is power-on.
+
+##sector2.asm
+Well, it's a simple grub-like program. I didn't finish the basics of a grub.
+It's just a practice. I will write a stage-2 booting code which will load the kernel. Yeah, next time, it will be another grub but more simple. Just for fun.
+
+## How to build and deploy
+I combine the making and deploying. You can use udisk.sh. 
+>==Note I assume your U-disk is marked as /dev/sdc. You can change that in udisk.sh.==
+
+In GNU Linux
+>$ sh ./udisk.sh
+
+You will prompt the sudo stuff.
+
+##Others
+boot code should be smaller than 446 bytes(include 446 bytes), that will not break up the DPT(Disk Patition Table, 64 bytes)
+
+Sector2 code is 512 bytes for simple. Larger than a sector will bring many problems. The size-free code will be next topic. 
+
+
+#Enjoy it, have fun!
---
+++ bootsector2/sector2.asm
@@ -0,0 +1,124 @@
+
+;;
+;; 简单的高斯计算，累加而已
+;;
+
+org 0x6000
+
+%include "inc/io.inc"
+	
+xor ax, ax
+mov ds, ax
+mov es, ax
+begin:
+	;;cls ;; some bios does not clear the PCI devices informations left on the screen.
+	mov si, menu
+	call puts
+	putc 13
+	putc 10
+init:
+	getc
+	cmp al, 13
+	je .ParseKey ;; equal to init.ParseKey
+	putc al
+	mov ah, 0
+	push ax	
+	jmp init
+.ParseKey:
+	putc 13
+	putc 10
+	pop ax
+	cmp al, 's' ;; Delete Key
+	je Start
+	cmp al, 'e'
+	je .PowerOff
+	cmp al, 'r'
+	je .Reset
+	cmp al, 'v'
+	je .Version
+	cmp al, 'c'
+	je .Clear
+	cmp al, 'm'
+	je .Menu
+	jmp init
+.PowerOff:
+	shutdown
+.Clear:
+	cls
+	jmp init:
+.Version:
+	call ShowVersion
+	jmp init
+.Reset:
+	jmp 0xFFFF:0000
+.Menu:
+	mov si, menu
+	call puts
+	jmp init
+
+DECLARE_IO_API
+
+section .text
+Start:
+;;	cls
+	
+	mov ax, 1 ;; operand
+	mov dx, 0 ;; sum
+	mov cx, 100
+
+ADD:
+	add dx, ax
+	inc ax
+	loop ADD
+
+PrintResult:
+	mov si, msg
+	call puts
+ 
+	mov ax, dx
+	call Disp2ByteInHex
+	
+	mov si, crlf
+	call puts
+
+End:
+	jmp begin
+	;jmp $
+;; ----------------------------------------------------------------------
+
+msg db '1+2+...+100='
+crlf db 13, 10, 0
+hr db '------------------------------------------------------------', 13, 10, 0
+version db 'Bootloader 0.1 by icecoobe', 13, 10, 0
+menu db `1).m:this menu\r\n2).s:start\r\n3).e:poweroff\r\n4).r:Reset\r\n5).c:clear screen\r\n6).v:version`, 13, 10, 0
+;; ----------------------------------------------------------------------
+
+ShowVersion:
+	mov si, hr
+	call puts
+	mov si, version
+	call puts
+	mov si, hr
+	call puts
+	ret
+
+func_strlen:
+	xor di, di
+	xor dx, dx
+	;; mov [length], ax
+	mov bp, ax
+.nextchar:
+	;; add ax, bx
+	mov dl, [bp+di]
+	cmp dl, 0
+	mov al, dl
+	je .loopend
+	inc di
+	loop .nextchar
+.loopend:
+	xor ax, ax
+	mov ax, di
+	ret 
+
+times (512-($-$$))	db 0 
+;db   0x55, 0xAA          ;2 byte boot signature
