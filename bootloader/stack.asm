b726c700ad9f24a9170dbb8c35df3541a0140a41
icecoobe <icecoobe@qq.com>
2015-08-14 17:57:54 +0800

    完善stack操作的实验，添加stack frame

--- bootloader/stack.asm
+++ bootloader/stack.asm
@@ -1,3 +1,40 @@
+;; About func calling and so-called stack frame.
+;;
+;; <High address>
+;;  ____________
+;; |			|
+;; |____________|
+;; |            | <--- sp after return
+;; |____________|___________________________________
+;; |	p1		|									|
+;; |____________|									|
+;; |	p2		|									|
+;; |____________|									O
+;; |	p3		|									n
+;; |____________|									e
+;; |	...		|									|
+;; |____________|									f
+;; |	pn		|									r
+;; |____________|									a
+;; |	(CS)	|									m
+;; |____________|									e
+;; |	IP		| <--- sp before return				|
+;; |____________|									|
+;; |	org-bp	| <--- current bp points to here	|
+;; |____________|									|
+;; |	loc-1	|									|
+;; |____________|									|
+;; |	loc-2	|									|
+;; |____________|									|
+;; |    ...     |									|
+;; |____________|									|
+;; |    loc-n   | <--- current sp points to here	|
+;; |____________|___________________________________|
+;; |            |
+;; |____________|
+;;
+;; 1) the one "sp before return" is return instruction pointer.
+;; 2) executing a far call, cs will be pushed onto stack, too.
 
 %include "inc/io.inc"
 
@@ -15,45 +52,30 @@ Init:
 	mov sp, 0xFFFF
 
 start:
-	;; mov ax, 0x3412
-	;; push ax
-	;; mov ax, 0x7856
-	;; call Disp2ByteInHex
-	;; mov ax, sp
-	;; call Disp2ByteInHex
-
-	;; pop ax
-	;; call Disp2ByteInHex
-	;; mov ax, sp
-	;; call Disp2ByteInHex
-
 	mov ax, 0x4142
-	;; push ax
-	;; pop ax
-	;; call Disp2ByteInHex
 	push ax
 	call func_putch
-	pop ax
+	;;pop ax
 
 	push 0x6062
 	call func_putch
-	pop ax
+
 End:
 	jmp $
 
-;; void func_putch(unsigned char c);
+;; void __stdcall func_putch(unsigned char c);
 func_putch:
-	;; pop ax
 	push bp
 	mov bp, sp
 	sub sp, 0x4
-	mov al, [bp + 4]
+	mov al, [bp + 4] ;; param-1. During far call, use [bp + 6]
 	mov ah, 0EH     
 	mov bx, 0007H   
 	int 10h
 	add sp, 0x4
 	pop bp
-	ret
+	ret 2	;; if ret n is used, we dont need fix stack after call.
+			;; the params pushed before will be cleaned.
 
 crlf db 0x0d, 0x0a
 times 510-($-$$) db 0
