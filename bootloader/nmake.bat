76884c694d16d3cd65cd85bf07dbf87e39b1779b
icecoobe <icecoobe@qq.com>
2015-08-21 10:20:56 +0800

    update nmake.bat for windows

--- bootloader/nmake.bat
+++ bootloader/nmake.bat
@@ -1,8 +1,10 @@
 
 @echo off
 
-nasm -f bin -o %1 %1.asm
+nasm -f bin -o bin\%1.bin %1.asm
 
-mbr %1
+rem .\bin\mbr bin\%1.bin
+bin\dd if=bin\%1.bin of="G:\VirtualBox VMs\dos2\dos2.vhd" bs=512 count=1 conv=notrunc
+
 
 vboxmanage startvm dos2
