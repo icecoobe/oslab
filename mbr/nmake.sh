f89e60b55a049cff1754062d5d4cbfc701b992f9
Luke Chen <coolchae@gmail.com>
2018-09-03 00:22:07 +0800

    remove duplicate mac osx script

--- mbr/nmake.sh
+++ mbr/nmake.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+if [ ! -d bin ]
+then
+	mkdir -v bin
+fi
 
 nasm -f bin -o bin/$1.bin $1.asm
 
-#~/c/mbr bin/$1.bin
-dd if=bin/$1.bin of="/media/luke/vm/VirtualBox VMs/dos2/dos2.vhd" bs=512 count=1 conv=notrunc
-
-vboxmanage startvm dos2
+dd if=bin/$1.bin of=bin/c.img seek=0 bs=512 count=1 conv=notrunc
+cd bin
+bochs -q -f bochsrc
